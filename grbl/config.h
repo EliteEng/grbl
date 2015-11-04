70cb53413e9b0b84bd8d7c78b612ef68cec4ba0b
Jim Morris <morris@wolfman.com>
2015-11-04 13:35:00 -0800

    Initial merge with upstream/grbl and ramps hack

--- grbl/config.h
+++ grbl/config.h
@@ -38,7 +38,7 @@
 
 // Default cpu mappings. Grbl officially supports the Arduino Uno only. Other processor types
 // may exist from user-supplied templates or directly user-defined in cpu_map.h
-#define CPU_MAP_ATMEGA2560_AZTEEGPRO // Arduino Uno CPU
+#define CPU_MAP_ATMEGA2560_RAMPS // mega 2560 and ramps (or azteegpro)
 //#define CPU_MAP_ATMEGA328P // Arduino Uno CPU
 
 // Define realtime command special characters. These characters are 'picked-off' directly from the
